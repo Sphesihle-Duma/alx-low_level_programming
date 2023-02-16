6cc6c8a7dc46a1450f3ca4d5109eec25fafc169b
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-16 14:06:45 -0800

    Addded 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char is: %c byte(s)\n", sizeof(b));
+	printf("Size of an int: %d byte(s)\n", sizeof(a));
+	printf("Size of a long int: %lu  byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %f byte(s)\n", sizeof(e));	
+	return(0);
+}
