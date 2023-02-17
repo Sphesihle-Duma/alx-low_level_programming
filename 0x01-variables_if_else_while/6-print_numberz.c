0f584362205d043c5ac453668512073de716e89c
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-17 13:59:40 -0800

    Added 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int test;
+
+	for (test = 48; test <= 57; test++)
+	{
+		putchar(test);
+	}
+	putchar('\n');
+	return (0);
+}
