ad001195d59974260ebda865ad10de831999c029
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-17 13:27:39 -0800

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		if (lower_case != 'q' || lower_case != 'e')
+			putchar(lower_case);
+	}
+	putchar('\n');
+	return (0);
+}
