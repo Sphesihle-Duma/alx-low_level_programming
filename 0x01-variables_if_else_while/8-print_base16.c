66fd13f683d52e78f7268d8928f461997066337b
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-17 14:34:10 -0800

    Added 8-print_base11.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int hex;
+
+	for (hex = 48; hex <= 57; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 97 ; hex <= 102; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
