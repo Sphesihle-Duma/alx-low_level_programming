ea6b070f88801ea28f05e09cae3ee243b0319c9a
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-22 14:12:38 -0800

    Added 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - prints first 500 fibonacci numbers
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int num1, num2, sum, temp, count;
+
+	num1 = 1;
+	num2 = 2;
+	count = 2;
+	printf("%d, %d\n", num1, num2);
+	while (count < 50)
+	{
+		sum = num1 + num2;
+		printf("%d, ", sum);
+		temp = num2;
+		num2 = sum;
+		num1 = temp;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
