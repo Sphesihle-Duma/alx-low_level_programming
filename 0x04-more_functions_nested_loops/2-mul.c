103f6a2889d6493cbf34a6f62144345debce73b1
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-23 11:45:45 -0800

    added 2-mul.c function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _mul - multiples two numbers
+  *
+  * @a: first number
+  * @b: second number
+  * Return: product of numbers
+  */
+int _mul(int a, int b)
+{
+	return (a * b);
+	_putchar('\n');
+}
