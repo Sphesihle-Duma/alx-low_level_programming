31f3c6ea2cda21344300f70a12abd9adf9140e6e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-23 13:22:36 -0800

    final fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,11 @@
   *
   * Return: prints 0 to 14
   */
-void more _numbers(void)
+void more_numbers(void)
 {
 	int i, j;
 
-	for (int i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
