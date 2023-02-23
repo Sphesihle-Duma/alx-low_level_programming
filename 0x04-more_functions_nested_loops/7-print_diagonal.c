f53d2508c636bd0a1fa13504de1229546e4ba19f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-23 14:40:41 -0800

    Added 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  * print_diagonal - prints backslashes diagonal
+  *
+  * @n: number of lines
+  * Return: prints backslashes
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			putchar('\n');
+		}
+	}
+}
