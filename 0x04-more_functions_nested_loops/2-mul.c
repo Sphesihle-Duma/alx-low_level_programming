5d49c2cda68fffaa2b8fe0323a4904c9a22972ef
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-23 11:49:24 -0800

    fixing mul function

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-  * _mul - multiples two numbers
+  * mul - multiples two numbers
   *
   * @a: first number
   * @b: second number
   * Return: product of numbers
   */
-int _mul(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 	_putchar('\n');
