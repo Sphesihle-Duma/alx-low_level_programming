6b6da0808e92ad299b612fc8ec2afd5c58fe6060
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-02-23 13:12:23 -0800

    added more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * more_numbers - prints numbers between 0 to 14
+  *
+  * Return: prints 0 to 14
+  */
+void more _numbers(void)
+{
+	int i, j;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
+			}
+			else
+			{
+				_putchar(j + 48);
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
