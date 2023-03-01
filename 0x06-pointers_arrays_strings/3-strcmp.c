e8712603d067a253c50bb1547979a53a67e28243
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-01 14:31:32 -0800

    final fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -21,11 +21,11 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (s1[i] > s2[i])
 		{
-			return (1);
+			return (s1[i] - s2[i]);
 		}
 		else if (s1[i] < s2[i])
 		{
-			return (-1);
+			return (s1[i] - s2[i]);
 		}
 	}
 	return (0);
