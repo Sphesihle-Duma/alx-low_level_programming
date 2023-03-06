31ef502a027f22aaa5ab10320d64d130972a5215
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-06 13:05:48 -0800

    fixing

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,17 +11,17 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	unsigned int j;
 
-	for (i = 0; *(accept + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*(accept + i) == s[j])
+			if (*(s + i) == accept[j])
 			{
 				count++;
 				break;
 			}
 		}
 	}
-	return (count + 1);
+	return (count);
 
 }
