34b2df0b88852e929724301076c6a8feb5672c74
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-10 15:09:40 -0800

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - Entry point
+  * @argc: number of args
+  * @argv: array of args
+  * Return: Always 0 on (Success)
+  */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
