aef12dee1b0c6ba04ff8f4f7f837cb8c8cc435b2
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-13 12:46:29 -0700

    final fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,12 @@ char *_strdup(char *str)
 	char *ptr;
 	int i;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; *(str + i) != '\0'; i++)
 		i++;
 	ptr = (char *)malloc(sizeof(char) * (i + 1));
-	if (ptr == NULL || str == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; *(str + i) != '\0'; i++)
 	{
