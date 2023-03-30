596f09bd33e36631da19b12c37e609c0b68b29c1
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-30 13:56:29 -0700

    Added print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * print_list - prints strings and their lengths
+  * @list: list
+  * Return: number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	unsigned int num_of_nodes = 0;
+
+	if (!h)
+		return (-1);
+	while (h)
+	{
+		
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		num_of_nodes++;
+		h = h->next;
+	}
+	return (num_of_nodes);
+}
