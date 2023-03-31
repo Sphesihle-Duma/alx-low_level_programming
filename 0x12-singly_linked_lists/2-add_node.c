a2c37b1f34f6822104dfa92b617f8bf7946c468c
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-03-31 11:53:24 -0700

    fixing

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * _strlen - function to calculate the length of a const char string
+ * @s: string to work with
+ * Return: length of string.
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	if (!s)
+		return (0);
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
   * add_node - insert an element in front of the first element
   * @head: pointer to the first element
   * @str: str
@@ -15,6 +31,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!newnode)
 		return (NULL);
 	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
 	if (!newnode->str)
 	{
 		free(newnode);
