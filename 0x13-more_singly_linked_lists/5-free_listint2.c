7a623a9e182abf1310580721120f99c78e06bfb5
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-04-04 11:55:05 -0700

    fixing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,13 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp1, *temp2;
 
-	temp1 = *head;
-	while (temp1)
+	if (head)
 	{
-		temp2 = temp1;
-		temp1 = temp1->next;
-		free(temp2);
+		temp1 = *head;
+		while (temp1)
+		{
+			temp2 = temp1;
+			temp1 = temp1->next;
+			free(temp2);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
-	head = NULL;
 }
