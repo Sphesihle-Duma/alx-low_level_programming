3e6b81dbd85f8bbf177296e67788e4c50a2b844e
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-04-04 11:37:19 -0700

    Added 5-free function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * free_listint2 - clears the memory that was previously
+  * allocated for list
+  * @head: pointer that points to the pointer that points the first
+  * node
+  * Return: Nothing
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp1, *temp2;
+
+	temp1 = *head;
+	while (temp1)
+	{
+		temp2 = temp1;
+		temp1 = temp1->next;
+		free(temp2);
+	}
+	head = NULL;
+}
