ed7261cfde3165c3445b88c45880731216e9ca8f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-04-10 17:57:45 -0700

    Added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * get_bit - searches for a bit at a specified index
+  * @n: number to be convert into binary
+  * @index: index of a bit
+  * Return: bit
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+	for (i = 0; i < 64; i++)
+	{
+		if (i == index)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
