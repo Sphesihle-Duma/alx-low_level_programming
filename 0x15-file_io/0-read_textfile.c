96d6f5a4040f5b7b4b476f36f7dfd5ca64d85ffa
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-04-11 12:02:21 -0700

    Added read_text

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * read_textfile - reads data from the file
+  * @filename: name of the file
+  * @letters: number of letters
+  * Return: number of bytes on Success
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int num_of_read, fd;
+	int num_of_write;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	num_of_read = read(fd, buf, letters);
+	if (num_of_read < 0)
+		return (0);
+	num_of_write = write(STDOUT_FILENO, buf, num_of_read);
+	free(buf);
+	return (num_of_write);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
