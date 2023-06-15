a5c8e29ae50a2909d6e318c541b997d23d84a29f
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-15 13:28:04 -0700

    fixing betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,11 +2,14 @@
 /**
  * print_dlistint - prints all elements
  * @h: pointer to the doubly linked lists
+ * Return: number of elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
+	while (h->prev)
+		h = h->prev;
 	while (h)
 	{
 		count++;
