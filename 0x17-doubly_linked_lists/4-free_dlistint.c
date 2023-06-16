69a9738ade4a020ce9f94618a7d76dd7c9abd3f6
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-16 05:07:07 -0700

    Added 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees the doubly list
+ * @head: doubly linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *backward = head;
+	dlistint_t *forward;
+
+	if (head)
+	{
+		while (backward->prev)
+			backward = backward->prev;
+		while (backward)
+		{
+			forward = backward->next;
+			free(backward);
+			backward = forward;
+		}
+		
+
+	}
+}
