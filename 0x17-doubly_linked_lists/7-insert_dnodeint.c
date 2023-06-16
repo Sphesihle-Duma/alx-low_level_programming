ee094b907ef9de1d9beb3db57b100cedd3fbcd83
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-16 10:59:45 -0700

    handling all egde cases test

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,14 +11,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h;
 	dlistint_t *previous, *new_node;
 	unsigned int i = 0;
-	const int new_n = n;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
-		return (add_dnodeint(h, new_n));
+		return (add_dnodeint(h, n));
 	if (*h)
 	{
 		while (current->prev)
@@ -38,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			current = current->next;
 		}
 		if (idx == i)
-			return (add_dnodeint_end(h, new_n));
+			return (add_dnodeint_end(h, n));
 	}
 	free(new_node);
 	return (NULL);
