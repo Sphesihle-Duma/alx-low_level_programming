0d8f79e46981613c7a5bc97919cbf994c64a3e19
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-16 03:50:12 -0700

    Fixing traversing issue

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,26 +8,23 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *h = *head;
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
 	if (*head == NULL)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (!new_node)
-			return (NULL);
 		new_node->next = NULL;
-		new_node->n = n;
 		new_node->prev = NULL;
 		*head = new_node;
 		return (*head);
 	}
-	while ((*head)->prev)
-		*head = (*head)->prev;
-	(*head)->prev = *head;
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
+	while (h->prev)
+		h = h->prev;
+	h->prev = new_node;
 	new_node->next = *head;
-	new_node->n = n;
 	new_node->prev = NULL;
 	*head = new_node;
 	return (new_node);
