eb4949fa10b0fa61c7d02fdc6d0106f08047c323
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-16 12:02:44 -0700

    handling adding a node at the end case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (i == idx)
 			{
 				if (current->next == NULL)
-					return (add_dnodeint_end(h, n))
+					return (add_dnodeint_end(h, n));
 				new_node = malloc(sizeof(dlistint_t));
 				if (!new_node)
 					return (NULL);
