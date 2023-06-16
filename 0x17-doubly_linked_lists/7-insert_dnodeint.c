0dc0c40a950f03a6d6e6d5cf519e1aa435c9d020
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-06-16 11:23:02 -0700

    handling insertion properly

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,12 +10,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current = *h;
 	dlistint_t *previous, *new_node;
-	unsigned int i = 0;
+	unsigned int i = 1;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (*h)
@@ -27,9 +23,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (i == idx)
 			{
+				new_node = malloc(sizeof(dlistint_t));
+				if (!new_node)
+					return (NULL);
 				previous = current->prev;
 				new_node->prev = previous;
 				new_node->next = current;
+				new_node->n = n;
 				previous->next = new_node;
 				return (new_node);
 			}
@@ -39,7 +39,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (idx == i)
 			return (add_dnodeint_end(h, n));
 	}
-	free(new_node);
 	return (NULL);
 }
 
