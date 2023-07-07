2f9bab320fb65ec798ecba47f9bbe7ac6cc20b96
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-07 03:45:14 -0700

    testing

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,12 +7,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	hash_node_t **array;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
-	new_hash_table = malloc(size * sizeof(hash_table_t));
-
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (!new_hash_table)
 		return (NULL);
+	array = malloc(size * sizeof(hash_node_t *));
+	if (!array)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+	new_hash_table->size = size;
+	new_hash_table->array = array;
 	return (new_hash_table);
 }
