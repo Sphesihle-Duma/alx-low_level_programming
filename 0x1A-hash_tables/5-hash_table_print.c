592d3a1117ba5e318dd9b1c88a61d743311f4f93
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-07 16:48:39 -0700

    handling the comma properly

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the keys and values of the hash table
+ *
+ * @ht: pointer to the hash table
+ * Return: no return
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
