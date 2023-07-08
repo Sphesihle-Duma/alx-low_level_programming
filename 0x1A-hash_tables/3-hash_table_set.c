f74ecbbf509762429c28991a17170152c4130874
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-08 07:18:30 -0700

    modifying 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,65 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table to add element into
- * @key: key to hash
- * @value: string associated with a key
- * Return: 1 on Success or 0 no fail
+ * add_n_hash - adds a node at the beginning of a hash at a given index
+ *
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
+ * Return: head of the hash
  */
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_element, *tmp;
+	hash_node_t *tmp;
 
-	if ((!key || strlen(key) == 0) || !ht)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_element = malloc(sizeof(hash_node_t));
-	if (!new_element)
-		return (0);
-	new_element->key = strdup(key);
-	new_element->value = strdup(value);
-	if (!new_element->key || !new_element->value)
-	{
-		free(new_element->key);
-		free(new_element->value);
-		free(new_element);
-	}
-	tmp = ht->array[index];
-	if (!tmp)
-	{
-		new_element->next = NULL;
-		ht->array[index] = new_element;
-		return (1);
-	}
-	while (tmp)
+	tmp = *head;
+
+	while (tmp != NULL)
 	{
 		if (strcmp(key, tmp->key) == 0)
 		{
 			free(tmp->value);
 			tmp->value = strdup(value);
-			if (tmp->value == NULL)
-				return (0);
-			return (1);
+			return (*head);
 		}
 		tmp = tmp->next;
 	}
-	new_element->next = ht->array[index];
-	ht->array[index] = new_element;
-	return (1);
 
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) to a given hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int k_index;
+
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
+
+	return (1);
 }
