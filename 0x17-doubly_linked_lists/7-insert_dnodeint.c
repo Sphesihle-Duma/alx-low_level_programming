f390d6eeb9c4a8158dfcb94f68fd25bff8835bbc
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-09 07:04:55 -0700

    added a special case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,6 +42,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (*h == NULL && idx != 0)
 		return (NULL);
+	if (*h == NULL && idx == 0)
+		add_dnodeint(h, n);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
