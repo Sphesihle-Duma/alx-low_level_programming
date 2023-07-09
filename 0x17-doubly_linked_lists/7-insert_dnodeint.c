d343b0117cbe1bd5a807c7d278b5bb3fd62c04dc
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-07-09 06:40:49 -0700

    handling insert in the middle

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,32 @@
 #include "lists.h"
 /**
+ * d_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t d_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+/**
  * insert_dnodeint_at_index - inserts a node at idx
  * @h: pointer to pointer
  * @idx: index of the node
@@ -8,37 +35,38 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h;
-	dlistint_t *previous, *new_node;
-	unsigned int i = 1;
+	dlistint_t *new_node, *temp;
+	unsigned int length, i = 0;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	if (*h)
+	if (!h)
+		return (NULL);
+	if (*h == NULL && idx != 0)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	while ((*h)->prev)
+		*h = (*h)->prev;
+	temp = *h;
+	length = d_len(*h);
+	while (temp && i < length)
 	{
-		while (current->prev)
-			current = current->prev;
-		*h = current;
-		while (current)
+		if (idx == 0 && i == 0)
+			add_dnodeint(h, n);
+		if (i == idx)
 		{
-			if (i == idx)
-			{
-				if (current->next == NULL)
-					return (add_dnodeint_end(h, n));
-				new_node = malloc(sizeof(dlistint_t));
-				if (!new_node)
-					return (NULL);
-				previous = current->prev;
-				new_node->prev = previous;
-				new_node->next = current;
-				new_node->n = n;
-				previous->next = new_node;
-				return (new_node);
-			}
-			i++;
-			current = current->next;
+			new_node->next = temp;
+			new_node->prev = temp->prev;
+			new_node->n = n;
+			temp->prev = new_node;
+			new_node->prev->next = new_node;
+			return (new_node);
 		}
+		i++;
+		temp = temp->next;
 	}
+	if (idx == length)
+		add_dnodeint_end(h, n);
 	return (NULL);
 }
 
