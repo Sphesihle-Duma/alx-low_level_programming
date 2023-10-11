79be6bfcad57078f9b08a0bc20484b713f7a02fa
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-11 13:45:10 -0700

    modified the code

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,54 +1,44 @@
-#include <math.h>
 #include "search_algos.h"
-/**
- * my_linear_search - performs linear search within a block of the array
- * @array: pointer to the first element of the array
- * @start: starting index of the block
- * @size: number of elements in the array
- * @value: value to search for
- * Return: the index where value is located, or -1 if not found
- */
-int my_linear_search(int *array, int start, size_t size, int value)
-{
-	int i;
-
-	for (i = start; i < (int)size; i++)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		else if (array[i] > value)
-			break;
-	}
-	return (-1);
-}
+#include <math.h>
 
 /**
- * jump_search - searches a value using jump search algo
- * @array: array to the tested
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
  * @size: size of the array
- * @value: value to be searched
- * Return: index of value on success otherwise -1
+ * @value: value to search in
+ * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int step = (int)sqrt(size);
-	int end, start = 0;
+	int index, m, k, prev;
 
-	if (size == 0 || array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
-	while(array[start] < value)
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		end = start + step;
-		if (end >= (int)size)
-			end = size;
-		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		if (array[start] <= value && array[end - 1] >= value)
-		{
-			printf("Value found between indexes [%d] and [%d]\n", start, end);
-				break;
-		}
-		start = end;
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-	return my_linear_search(array, start, size, value);
+
+	return (-1);
 }
